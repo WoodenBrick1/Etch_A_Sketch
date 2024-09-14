9692e9842ad35fe6f0a7a0c1db51fb4027f3f7af
WoodenBrick1 <brickwooden@gmail.com>
2024-09-14 18:00:05 +0200

    Added the input functionality and the ability to resize the canvas

--- java.js
+++ java.js
@@ -1,21 +1,27 @@
 
-
-
 const container = document.getElementById("container");
-const size = 16;
-let gridSquares = [];
+const input = document.getElementById("input-size");
+const btnSize = document.getElementById("btn-size");
 
+const sizeStart = 16;
 
 window.onload = () => {
-  let squareSize = container.offsetWidth / size;
+  let squareSize = container.offsetWidth / sizeStart;
   console.log(container.offsetWidth)
+
+  loadGrid(squareSize, sizeStart);
+}
+
+
+
+const loadGrid = (squareSize, size) =>
+{
   for (let i = 0; i < size * size; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
     grid.style.width = `${squareSize}px`;
     grid.style.height = `${squareSize}px`;
     
-    gridSquares.push(grid)
     container.appendChild(grid);
 
 
@@ -25,8 +31,30 @@ window.onload = () => {
         event.target.style.backgroundColor = "black";
       }
     });
-      
-      
+    
   }
 }
 
+
+const changeGrid = () =>
+{
+  const size = parseInt(input.value);
+  input.value = "";
+
+  if (size >= 1 && size <= 100)
+  {
+     container.innerHTML = "";
+     loadGrid(container.offsetWidth / size, size);
+  }
+}
+
+
+input.addEventListener("keydown", (e) =>
+{
+  if (e.key == "Enter")
+  {
+    changeGrid();
+  }
+})
+
+btnSize.addEventListener("click", changeGrid)
