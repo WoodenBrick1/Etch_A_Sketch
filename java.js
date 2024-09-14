e51c75ceadb11608a284875c54a0f08e59ffc7c4
WoodenBrick1 <brickwooden@gmail.com>
2024-09-14 16:59:37 +0200

    Managed to add the grid, and the ability to hover

--- java.js
+++ java.js
@@ -1,10 +1,32 @@
-const container = document.querySelector(".container");
-const size = 16;
 
-for (let i = 0; i < size * size; i++) {
-  const grid = document.createElement("h3");
-  grid.classList.add("grid");
-  grid.textContent = "test";
-  container.appendChild(grid);
+
+
+const container = document.getElementById("container");
+const size = 16;
+let gridSquares = [];
+
+
+window.onload = () => {
+  let squareSize = container.offsetWidth / size;
+  console.log(container.offsetWidth)
+  for (let i = 0; i < size * size; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.style.width = `${squareSize}px`;
+    grid.style.height = `${squareSize}px`;
+    
+    gridSquares.push(grid)
+    container.appendChild(grid);
+
+
+
+    container.addEventListener("mouseover", (event) => {
+      if (event.target.classList.contains("grid")) {
+        event.target.style.backgroundColor = "black";
+      }
+    });
+      
+      
+  }
 }
 
